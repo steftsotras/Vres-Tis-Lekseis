5584ecb1123f218cb2aad966fc2c6775026e304c
Stefanos Tsotras <steftsotras2@gmail.com>
2019-04-11 23:26:00 +0300

    Add files via upload

---
+++ VresTisLekseis.java
@@ -0,0 +1,18 @@
+//ASKHSH 3
+//Tsotras Stefanos icsd13189
+package vrestislekseis;
+
+/**
+ *
+ * @author Stefanos
+ */
+public class VresTisLekseis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gameplay game =new Gameplay();
+    }
+    
+}
